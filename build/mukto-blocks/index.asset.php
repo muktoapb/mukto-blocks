82708cb643a94fe0f3b43730a64daa2ddee92f20
Mukto <muktoapb@gmail.com>
2025-02-24 11:26:56 +0600

    first commit

---
+++ build/mukto-blocks/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3b1bdb6c2c1d2c9c8767');
---
+++ build/mukto-blocks/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4ea1cbd3db66c5cde339');
